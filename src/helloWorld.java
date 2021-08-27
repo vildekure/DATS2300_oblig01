b7019766e9b7127e4a8d1ed71ab679b7c40fbd7e
vildekure <v-kure@hotmail.com>
2021-08-27 13:53:36 +0200

    Initial Commit

---
+++ src/helloWorld.java
@@ -0,0 +1,14 @@
+public class helloWorld {
+    public static void main (String[] args){
+
+        System.out.println("Hello world");
+        String n1 = "Marianne ";
+        String n2 = "Nina ";
+        String n3 = "Vilde ";
+        System.out.println("Hello " + n1 + n2 + "og " + n3);
+
+        for (int i = 1; i <= 10; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
